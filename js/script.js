f4d7feda8463abdf2fe32ca0078a0608751d8a44
samuelcarlson18 <99274631+samuelcarlson18@users.noreply.github.com>
2022-04-27 23:07:46 +0000

    code

--- js/script.js
+++ js/script.js
@@ -6,28 +6,28 @@
 
 'use strict'
 /**
- * This function calculates the area of a trapazoid.
+ * This function calculates gold handicap index.
  */
 function calculate () {
-  // input
+  // input best score
   let score1 = parseInt(document.getElementById('score1').value)
-  // input
+  // input second best score
   let score2 = parseInt(document.getElementById('score2').value)
-  // input
+  // input third best score
   let score3 = parseInt(document.getElementById('score3').value)
-  // input
+  // input fourth best score
   let score4 = parseInt(document.getElementById('score4').value)
-  // input
+  // input fifth best score
   let score5 = parseInt(document.getElementById('score5').value)
-  // input
+  // input sixth best score
   let score6 = parseInt(document.getElementById('score6').value)
-  // input
+  // input seventh best score
   let score7 = parseInt(document.getElementById('score7').value)
-  // input
+  // input eighth best score
   let score8 = parseInt(document.getElementById('score8').value)
-  // process
+  // process formula to calculate handicap index
   const handicap = (score1+score2+score3+score4+score5+score6+score7+score8)/8-72
 
-  // output
+  // output display users handicap index
   document.getElementById('handicap').innerHTML = 'You have a ' + handicap.toFixed(2) + ' golf handicap index. '
 }
